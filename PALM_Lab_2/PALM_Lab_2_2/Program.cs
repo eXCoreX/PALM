adc91c005bfbc117b88e9287d349cffb43eb2940
Rostyslav <excoreky@gmail.com>
2020-03-10 13:40:42 +0200

    basic project

---
+++ PALM_Lab_2/PALM_Lab_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PALM_Lab_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

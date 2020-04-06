e71e6b545663c40ad08be70b5f6bf342c4c34e9e
Rostyslav <excoreky@gmail.com>
2020-04-06 17:39:51 +0300

    + Base of Lab 3

---
+++ PALM_Lab_2/PALM_Lab_2_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PALM_Lab_2_3
+{
+    struct MyTime
+    {
+        int hour, minute, second;
+
+        public MyTime(int h, int m, int s)
+        {
+            hour = h;
+            minute = m;
+            second = s;
+        }
+
+        public override string ToString()
+        {
+            return $"{hour}:{minute:00}:{second:00}";
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MyTime t = new MyTime(9, 2, 30);
+            Console.WriteLine(t);
+        }
+    }
+}

4eb39f80cf23540e718a37a26b65d4f69aab2c91
eXCore <excoreky@gmail.com>
2020-04-25 04:58:52 +0300

    Lab 3 - redundant modulo

--- PALM_Lab_2/PALM_Lab_2_3/Program.cs
+++ PALM_Lab_2/PALM_Lab_2_3/Program.cs
@@ -30,7 +30,7 @@ namespace PALM_Lab_2_3
             if (second < 0)
                 second += 60 * 60 * 24;
             minute = (second / 60) % 60;
-            hour = (second / (60 * 60)) % 24;
+            hour = second / (60 * 60);
             second %= 60;
         }
     }

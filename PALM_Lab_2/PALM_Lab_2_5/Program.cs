d3309abc67a5330b25f73962b374888ccf8a08e2
eXCore <excoreky@gmail.com>
2020-05-06 15:34:49 +0300

    Lab 5 - ReadStudents

--- PALM_Lab_2/PALM_Lab_2_5/Program.cs
+++ PALM_Lab_2/PALM_Lab_2_5/Program.cs
@@ -1,14 +1,30 @@
 ﻿// Rostyslav L. Lab 5 Var 10
 
 using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace struct_lab_student
 {
     class Program
     {
+        static List<Student> ReadStudents(string filepath)
+        {
+            string input = File.ReadAllText(filepath);
+            string[] inputLines = input.Split('\r', '\n');
+            List<Student> students = new List<Student>(inputLines.Length);
+            for (int i = 0; i < inputLines.Length; i++)
+            {
+                students[i] = new Student(inputLines[i]);
+            }
+            return students;
+        }
+
         static void Main(string[] args)
         {
-            
+            List<Student> students = ReadStudents("input.txt");
+
+
         }
     }
 }
